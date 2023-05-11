6e728679379ced8feeec6a907205251c667345f2
amansour13 <a.mansour.dev1@gmail.com>
2023-05-11 21:13:45 +0300

    second half design

--- src/App.js
+++ src/App.js
@@ -1,35 +1,141 @@
 import './App.css';
-import image from './logo180.png';
+import image1 from './images/New folder/icon-online.svg';
+import image2 from './images/New folder/icon-budgeting.svg';
+import image3 from './images/New folder/icon-onboarding.svg';
+import image4 from './images/New folder/icon-api.svg';
+import image5 from './images/image-currency.jpg';
+import image6 from './images/image-restaurant.jpg';
+import image7 from './images/image-plane.jpg';
+import image8 from './images/image-confetti.jpg';
+import image9 from './images/New folder/logo2.svg';
 
-const logo = {
-  name: 'logo 180',
-  imageUrl: './logo180.png',
-  imageSize: 90,
-};
+function Section2(){
+  return(
+
+    <div class="Container">
+    <div className='Container_section-2'>
+        <div className='s1 item'>
+            <img src={image1} alt='online banking icon'/>
+            
+            <h2>Online Banking</h2>
+            
+            <p className='txt'>Our modern web and mobile applications allow you to keep track of your finances 
+                wherever you are in the world.</p>
+        </div>
+        <div className='s2 item'>
+            <img src={image2} alt='budgeting icon'/>
+            
+            <h2>Simple Budgeting</h2>
+            
+            <p className='txt'>See exactly where your money goes each month. Receive notifications when you’re 
+                close to hitting your limits.</p>
+
+        </div>
+        <div className='s3 item'>
+            <img src={image3} alt="onboarding icon"/>
+            
+            <h2>Fast Onboarding</h2>
+            
+            <p className="txt">We don’t do branches. Open your account in minutes online and start taking control 
+                of your finances right away.</p>
+
+        </div>
+        <div className="s4 item">
+            <img src={image4} alt="api icon"/>
+            
+            <h2>Open API</h2>
+            
+            <p className="txt">Manage your savings, investments, pension, and much more from one account. Tracking 
+                your money has never been easier.</p>
+        </div> 
+    </div>
+    </div>
+  );
+}
+
+function Section3(){
+  return(
+    <div class="Container">
+    <div className='section-3'>
+            <h1>Latest Articles</h1>
+
+            <div className="Container_section-3">
+                <div className="s1 item">
+                    <img src={image5} alt="currency" />
+                    <h5 className="peot">By Claire Robinson</h5>
+                    <h5>Receive money in any currency with no fees</h5>
+                    <p className="txt">The world is getting smaller and we’re becoming more mobile. So why should you be 
+                        forced to only receive money in a single …</p>
+                </div>
+
+                <div className="s2 item">
+                    <img src={image6} alt="restaurant"/>
+                    <h5 className="peot">By Wilson Hutton</h5>
+                    <h5>Treat yourself without worrying about money</h5>
+                    <p className="txt">Our simple budgeting feature allows you to separate out your spending and set 
+                        realistic limits each month. That means you …</p>
+                </div>
+
+                <div className="s3 item">
+                    <img src={image7} alt="plane"/>
+                    <h5 className="peot">By Wilson Hutton</h5>
+                    <h5>Take your Easybank card wherever you go</h5>
+                    <p className="txt">We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
+                        while you’re abroad. We’ll even show you …</p>
+                </div>
+
+                <div className="s4 item">
+                    <img src={image8} alt="confetti"/>
+                    <h5 className="peot">By Claire Robinson</h5>
+                    <h5>Our invite-only Beta accounts are now live!</h5>
+                    <p className="txt">After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
+                        It’s easy to request an invite through the site …</p>
+                </div>
+            </div>
+            
+        </div>
+        </div>
+  );
+}
+
+function Footer (){
+  return(
+    <div className="fcontainer">
+        <div className="footer">
+            <div className="s1 item"><img src={image9} alt=""/></div>
+            <div className="s2 item">
+                
+                <ul>
+                    <li><a href="/aboutus">About us</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                </ul>
+                </div>
+                <div className="s3 item">
+                <ul>
+                    <li><a href="/careers">Careers</a></li>
+                    <li><a href="/support">Support</a></li>
+                    <li><a href="/privacypolicy">Privacy Policy</a></li>
+                </ul>
+
+            </div>
+            <div className="s4 item">
+                <input type="button" value="Request invite" className="btn_request"/>
+                <h5>© Easybank. All Rights Reserved</h5>
+            </div>
+
+        </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <nav>
-      <img
-        className="logo"
-        src={image}
-        alt={'Photo of' + logo.name}
-        style={{
-          width: logo.imageSize,
-          height: logo.imageSize
-        }}
-      />
-        <ul className='navbar'>
-          <li><a href='#' className='links active'>Home</a></li>
-          <li><a href='#' className='links'>Events</a></li>
-          <li><a href='#' className='links'>About Us</a></li>
-        </ul>
-      </nav>
-      <div> 
-        
-      </div>
-    </div>
+    <>
+      <Section2 />
+      <Section3 />
+      <Footer />
+    </>
   );
 }
 
