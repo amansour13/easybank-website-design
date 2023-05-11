9ae87179153b9191b7cf48eb0a6ddc4d83287504
muhamed6 <muhamedashraf990@gmail.com>
2023-05-11 21:29:28 +0300

    nav and first section

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
 import './App.css';
 import image from './logo180.png';
-
-const logo = {
-  name: 'logo 180',
-  imageUrl: './logo180.png',
-  imageSize: 90,
-};
-
+import logo1 from'./icon-hamburger.svg';
+import logo2 from'./image-mockups.png';
+import logo3 from'./icon-online.svg';
+import logo4 from'./logo.svg';
 function App() {
   return (
-    <div className="App">
-      <nav>
-      <img
-        className="logo"
-        src={image}
-        alt={'Photo of' + logo.name}
-        style={{
-          width: logo.imageSize,
-          height: logo.imageSize
-        }}
-      />
-        <ul className='navbar'>
-          <li><a href='#' className='links active'>Home</a></li>
-          <li><a href='#' className='links'>Events</a></li>
-          <li><a href='#' className='links'>About Us</a></li>
-        </ul>
-      </nav>
-      <div> 
-        
-      </div>
+    <div className="App"> 
+    <div class="container">
+          <img src={logo4} class="a" alt="photo"  />  
+        <div className="one">
+            {/* <!-- <h1 class="f"> ARSHA</h1> --> */}
+            <ul class="naaaaav">
+                <li class="firsssssssst"> Home</li>
+                <li class="firsssssssst"> About</li>
+                <li class="firsssssssst"> Contact</li>
+                <li class="item"> Blog</li>
+                <li class="firsssssssst"> Careers</li>
+               
+            </ul>
+            <button class="firstbuttttton" >Request invite</button>
+            <img src={logo1} class="b"  alt="photo" />
+        </div>
+    
+    </div>
+    <div class="big">
+<div class="first">
+<h1>  Next generation</h1><h1 class="secooond"> digital banking</h1>
+<p class="l"> Take your financial life online. Your Easybank account will be a one-</p><p>stop-shop 
+    for spending, saving, budgeting, investing, and much more.</p>
+<button class="ww"  >Request invite</button>
+</div>
+<div class="second">
+    <img src={logo2}   alt="photo"  />
+</div>
+    </div>
+    <h1 class="seconddddd"> Why choose Easybank?</h1>
+    <p class="seconddddd">We leverage Open Banking to turn your bank account into your financial hub.</p>
+    <p class="seconddddd">Control 
+        your finances like never before.</p>
+    {/* <div class="bigone">
+        <div class="oneeeee">
+            <img src={logo3}alt="photo"  />
+            <h1>Online Banking</h1>
+            <p class="eee"> Our modern web and mobile applications allow you to keep track of your finances 
+                wherever you are in the world.</p>
+            </div>
+</div> */}
     </div>
   );
 }
